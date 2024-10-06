f1bcc176571cfb9806c3fb71933776769b618ef9
Tamar Noselidze <takonoselidze@skirit.metacentrum.cz>
2024-10-06 13:18:46 +0200

    updating job script

--- job.sh
+++ job.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
-#PBS -q gpu@meta-pbs.metacentrum.cz
+#PBS -q gpu@pbs-m1.metacentrum.cz
 #PBS -l walltime=4:00:00
-#PBS -l select=1:ncpus=1:ngpus=1:mem=10gb:scratch_local=100gb
+#PBS -l select=1:ncpus=1:ngpus=1:mem=30gb:scratch_local=100gb
 
 trap 'clean_scratch' TERM EXIT
 
 cd $SCRATCHDIR
 
 HOME_DIR='/storage/brno2/home/takonoselidze'
-PROJECT_DIR='/storage/brno2/home/takonoselidze/Random_Position_Patch'
-DATASET_DIR='/storage/brno2/home/takonoselidze/imagenetv2-top-images'
+PROJECT_DIR='/storage/brno2/home/takonoselidze/Thesis/Random_Position_Patch'
+DATASET_DIR='/storage/brno2/home/takonoselidze/Thesis/imagenetv2-top-images'
 
 
 # Copy project and data to the scratch directory
-cp -r $HOME_DIR/sing.sh $SCRATCHDIR
+cp -r $HOME_DIR/Thesis/sing.sh $SCRATCHDIR
 cp -r $PROJECT_DIR/* $SCRATCHDIR
-cp -r $DATASET_DIR/* $SCRATCHDIR
+#cp -r $DATASET_DIR/* $SCRATCHDIR
+cp -r $DATASET_DIR $SCRATCHDIR
 
+#for debugging
+ls -R $SCRATCHDIR
 
 # Make directories for outputs
-mkdir $SCRATCHDIR/results
-mkdir $SCRATCHDIR/logs
+mkdir -p $SCRATCHDIR/results
+mkdir -p $SCRATCHDIR/logs
 
 
 # Run the singularity container and execute singularity.sh inside it
-singularity exec --nv /storage/brno2/home/takonoselidze/containers/pytorch_with_libraries.sif bash sing.sh
+singularity exec --nv /storage/brno2/home/takonoselidze/containers/pytorch_container.sif bash sing.sh
 
 
 # Archive the results
@@ -32,4 +35,4 @@ tar -czf results.tar.gz $SCRATCHDIR/results $SCRATCHDIR/logs
 
 
 # Copy results back to the storage directory
-cp results.tar.gz $DATADIR/results || export CLEAN_SCRATCH=false
+cp results.tar.gz $HOME_DIR/results || export CLEAN_SCRATCH=false
--- sing.sh
+++ sing.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y python3-pip
+#apt-get update
+#apt-get install -y python3-pip
 
-pip3 install torch torchvision matplotlib numpy Pillow
+#pip3 install torch torchvision matplotlib numpy Pillow
+pip3 install --user torch torchvision matplotlib numpy Pillow
 
 echo "Running Python script"
 python3 main.py
