79d8886212244ca77a4ad4e2d013634227a92ce2
Tamar Noselidze <takonoselidze@gmail.com>
2024-11-10 22:30:18 +0100

    adding ensemble logic

--- Random_Position_Patch/main.py
+++ Random_Position_Patch/main.py
@@ -1,4 +1,5 @@
 import os, argparse, random, numpy
+import wandb
 
 import torch
 import torch.optim as optim
@@ -8,6 +9,7 @@ torch.autograd.set_detect_anomaly(True)
 
 from deployer import Deployer
 from loss import AdversarialLoss
+from generator import Generator
 
 from torchvision.models.resnet import resnet50, ResNet50_Weights, resnet152, ResNet152_Weights
 from torchvision.models import vit_b_16, ViT_B_16_Weights, vit_l_16, ViT_L_16_Weights, vit_b_32, ViT_B_32_Weights, vgg16_bn, VGG16_BN_Weights, swin_b, Swin_B_Weights
@@ -32,17 +34,7 @@ def adjust_brightness(image, brightness_factor):
     adjusted_image = brightness_transform(image)
     return adjusted_image
 
-def start(project_name, device, generator, optimizer, deployer, discriminators, attack_type, dataloader, num_of_classes, num_of_epochs=40, brightness_factor=None, color_transfer=None, input_dim=100):
-    import wandb
-
-    # Initialize W&B
-    wandb.init(project=project_name, entity='takonoselidze-charles-university', config={
-        'epochs': num_of_epochs,
-        'classes' : num_of_classes,
-        'attack_type': 'Including the image embeddings' if attack_type=='0' else 'Without the image embeddings',
-        'input_dim': input_dim
-    })
-
+def start(device, generator, optimizer, deployer, discriminators, dataloader, num_of_classes, num_of_epochs=40, brightness_factor=None, color_transfer=None, input_dim=100):
 
     best_epoch_asr = 0  
     best_epoch_images = {}
@@ -77,18 +69,18 @@ def start(project_name, device, generator, optimizer, deployer, discriminators, 
             modified_images = []
             adv_patches = None
 
-            if attack_type == '0':
-                adv_patches = generator(noise)
-            else:
-                # generating patches for each image
-                adv_patches = []  # we need to store generated patch for each image
+            # if attack_type == '0':
+            adv_patches = generator(noise)
+            # else:
+            #     # generating patches for each image
+            #     adv_patches = []  # we need to store generated patch for each image
 
-                for i in range(batch_size):
-                    # print(f'*************** calling from start')
-                    adv_patch = generator(noise[i].unsqueeze(0).to(device), images[i].unsqueeze(0).to(device))
-                    adv_patches.append(adv_patch)
+            #     for i in range(batch_size):
+            #         # print(f'*************** calling from start')
+            #         adv_patch = generator(noise[i].unsqueeze(0).to(device), images[i].unsqueeze(0).to(device))
+            #         adv_patches.append(adv_patch)
 
-                adv_patches = torch.cat(adv_patches, dim=0).to(device)  # Stack generated patches
+            #     adv_patches = torch.cat(adv_patches, dim=0).to(device)  # Stack generated patches
 
             # print(f"   Generated adversarial patch: {adv_patches[0].cpu()}")  # Log a generated patch
 
@@ -203,11 +195,39 @@ def start(project_name, device, generator, optimizer, deployer, discriminators, 
         print(f'|___ ASR of the best performing batch: {best_batch_asr * 100:.2f}%')
 
     print(f'\n\n-- Average ASR over all {num_of_epochs} epochs: {total_asr / num_of_epochs * 100:.2f}% --\n\n')
+    print(f'\n\nResults saved.\nBest ASR achieved over {num_of_epochs} epochs: {best_epoch_asr * 100:.2f}%')
+
+    return best_epoch_images
 
 
+def get_models(model_names):
+    models = []
+    for model_name in model_names:
+        if model_name == 'resnet50':
+            model = resnet50(weights=ResNet50_Weights.DEFAULT)
+        elif model_name == 'resnet152':
+            model = resnet152(weights=ResNet152_Weights.DEFAULT)
+        elif model_name == 'vgg16_bn':
+            model = vgg16_bn(weights=VGG16_BN_Weights.DEFAULT)
+        elif model_name == 'vit_b_16':
+            model = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
+        elif model_name == 'vit_b_32':
+            model = vit_b_32(weights=ViT_B_32_Weights.DEFAULT)
+        elif model_name == 'vit_l_16':
+            model = vit_l_16(weights=ViT_L_16_Weights)
+        elif model_name == 'swin_b':
+            model = swin_b(weights=Swin_B_Weights)
+    
+        model.to(device)
+        model.eval()
+        models.append(model)
 
+    return models
+
+
+def display_images(images):
     i=0
-    for original, modified in best_epoch_images.items():
+    for original, modified in images.items():
         i+=1    
         image_key = f'best_epoch_img_{i}'
           
@@ -217,38 +237,46 @@ def start(project_name, device, generator, optimizer, deployer, discriminators, 
          })
         # print(f'displayed: original and modified of {image_key}')
 
-    print(f'\n\nResults saved.\nBest ASR achieved over {num_of_epochs} epochs: {best_epoch_asr * 100:.2f}%')
+       
+    # wandb.finish()
 
-        
-    wandb.finish()
 
-def get_model(model_name):
-    if model_name == 'resnet50':
-        model = resnet50(weights=ResNet50_Weights.DEFAULT)
-    elif model_name == 'resnet152':
-        model = resnet152(weights=ResNet152_Weights.DEFAULT)
-    elif model_name == 'vgg16_bn':
-        model = vgg16_bn(weights=VGG16_BN_Weights.DEFAULT)
-    elif model_name == 'vit_b_16':
-        model = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
-    elif model_name == 'vit_b_32':
-        model = vit_b_32(weights=ViT_B_32_Weights.DEFAULT)
-    elif model_name == 'vit_l_16':
-        model = vit_l_16(weights=ViT_L_16_Weights)
-    elif model_name == 'swin_b':
-        model = swin_b(weights=Swin_B_Weights)
+def transfer_to_target_models(models, images, target_class):
+    total_images = len(images)
     
-    # model.to(device)
-    model.eval()
-    return model
+    for model in models:
+        correctly_misclassified = 0
+        
+        for original, modified in images.items():
+            output = model(modified.unsqueeze(0))  
+            _, predicted = torch.max(output.data, 1)
+            
+            if predicted.item() == target_class:
+                correctly_misclassified += 1
+        
+        # Calculate ASR (Attack Success Rate)
+        avg_asr = (correctly_misclassified / total_images) * 100
+        print(f"Model: {model.__class__.__name__}")
+        print(f"Correctly misclassified: {correctly_misclassified}/{total_images}")
+        print(f"Avg ASR: {avg_asr:.2f}%\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Random Position Patch Attack')
 
-    parser.add_argument('--attack_type', choices=['0', '1'], help='0 for the normal patch generation, 1 to include image embedding into the patch training')
-    parser.add_argument('--image_folder_path', help='Image dataset to perturb', default='./imagenetv2-top-images/imagenetv2-top-images-format-val')
-    parser.add_argument('--model', choices=['resnet50', 'resnet152', 'vgg16_bn', 'vit_b_16', 'vit_b_32', 'vit_l_16', 'swin_b'], help='Model to attack')
+    # parser.add_argument('--attack_type', choices=['0', '1'], help='0 for the normal patch generation, 1 to include image embedding into the patch training')
+    parser.add_argument('--image_folder_path', help='Image dataset to perturb', default='../imagenetv2-top-images/imagenetv2-top-images-format-val')
+    # Transferability options
+    parser.add_argument('--transfer_mode', choices=['source-to-target', 'ensemble', 'cross-validation'], 
+                        help='Choose the transferability approach: source-to-target, ensemble, or cross-validation', default='source-to-target')
+    # parser.add_argument('--model', choices=['resnet50', 'resnet152', 'vgg16_bn', 'vit_b_16', 'vit_b_32', 'vit_l_16', 'swin_b'], help='Model to attack')
+    # List of models for ensemble or cross-validation mode
+    parser.add_argument('--training_models', nargs='+', 
+                        choices=['resnet50', 'resnet152', 'vgg16_bn', 'vit_b_16', 'vit_b_32', 'vit_l_16', 'swin_b'],
+                        help='List of models for ensemble or cross-validation. Use space to separate models, e.g., --model_list resnet50 vit_b_16 swin_b')
+    parser.add_argument('--target_models', nargs='+', 
+                    choices=['resnet50', 'resnet152', 'vgg16_bn', 'vit_b_16', 'vit_b_32', 'vit_l_16', 'swin_b'],
+                    help='List of models to attack and validate trained attack on. Use space to separate models, e.g., --model_list resnet50 vit_b_16 swin_b')
     parser.add_argument('--patch_size', choices=['48', '64', '80'], help='Size of the adversarial patch')
     parser.add_argument('--epochs', help='Number of epochs')
     parser.add_argument('--brightness', help='Brightness level for the patch')
@@ -259,15 +287,37 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    attack_type = args.attack_type
-    if attack_type == '0':            # without image 
-        from generator import Generator
-    elif attack_type == '1':
-        from gen import Generator
+    # attack_type = args.attack_type
+    # if attack_type == '0':            # without image 
+    #     from generator import Generator
+    # elif attack_type == '1':
+    #     from gen import Generator
+    # else:
+    #     raise ValueError('Invalid attack type. \nOptions: 0, 1.')
+
+
+    
+    transfer_mode = args.transfer_mode
+    training_model_names = args.training_models
+    target_model_names = args.target_models
+    intra_model_attack = False
+    cross_validation = False
+    if training_model_names is None:
+        raise ValueError('You should specify training models.')  
+    
+    if transfer_mode == 'source-to-target':
+        if target_model_names is None:
+            intra_model_attack = True
     else:
-        raise ValueError('Invalid attack type. \nOptions: 0, 1.')
+        if transfer_mode == 'cross-val':
+            cross_validation = True
+        if target_model_names is None:
+            raise ValueError("For the ensemble/cross-validation attack you should specify target models.")
+        
+
 
     patch_size = args.patch_size
+
     try:
         brightness_factor = float(args.brightness)
     except:
@@ -286,16 +336,17 @@ if __name__ == "__main__":
     generator.train()
     deployer = Deployer()
 
-    # model_name = args.model
-    # discriminator = get_model(model_name).to(device)  # moving model to the appropriate device
-    # discriminator.eval()
+    # # model_name = args.model
+    # # discriminator = get_model(model_name).to(device)  # moving model to the appropriate device
+    # # discriminator.eval()
 
-    model_names = ["resnet50", "resnet152"]
-    discriminators = []
-    for model_name in model_names:
-        discriminator = get_model(model_name).to(device)
-        discriminator.eval()
-        discriminators.append(discriminator)
+    # # model_names = ["resnet50", "resnet152"]
+    # discriminators = []
+    # for model_name in training_model_names:
+    #     discriminator = get_model(model_name).to(device)
+    #     discriminator.eval()
+    #     discriminators.append(discriminator)
+    discriminators = get_models(training_model_names)
 
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -312,7 +363,25 @@ if __name__ == "__main__":
 
     print(f'Using device: {device}')
     print(f"Generator device: {next(generator.parameters()).device}")
-    print(f"Discriminator device: {next(discriminator.parameters()).device}")
+    # print(f"Discriminator device: {next(discriminator.parameters()).device}")
     
-    project_name = f'Random Position Patch_tmp_{args.attack_type}-{args.model}{ "(br "+str(brightness_factor)+")" if brightness_factor else ""}{ "_col-tr"+color_transfer if color_transfer else ""}'
-    start(project_name, device, generator, optimizer, deployer, discriminators, attack_type, dataloader, num_of_classes, num_of_epochs, brightness_factor, color_transfer)
+    project_name = f'Random Position Patch_tmp_{ "(br "+str(brightness_factor)+")" if brightness_factor else ""}{ "_col-tr"+color_transfer if color_transfer else ""}'
+
+
+    # Initialize W&B
+    wandb.init(project=project_name, entity='takonoselidze-charles-university', config={
+        'epochs': num_of_epochs,
+        'classes' : num_of_classes,
+        # 'attack_type': 'Including the image embeddings' if attack_type=='0' else 'Without the image embeddings',
+        'input_dim': input_dim
+    })
+
+    best_ep_imgs = start(device, generator, optimizer, deployer, discriminators, dataloader, num_of_classes, num_of_epochs, brightness_factor, color_transfer)
+    display_images(best_ep_imgs)
+
+    if target_model_names is not None:
+        target_models = get_models(target_model_names)
+        transfer_to_target_models(target_models, best_ep_imgs, target_class=813)
+
+
+    wandb.finish()
