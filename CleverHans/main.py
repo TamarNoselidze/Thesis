ec42f1772937d85a3373d281f077009e55b3f031
Tamar Noselidze <takonoselidze@gmail.com>
2024-11-23 18:16:43 +0100

    updating cleverhans attack parameters

--- CleverHans/main.py
+++ CleverHans/main.py
@@ -38,23 +38,18 @@ def denormalize(image_tensor):
 def get_attack_info(attack_name, epsilon):
     attack = None
     attack_params = {}
-
     if attack_name == 'FGSM':
         attack = fast_gradient_method
-        # epsilon, np.inf, targeted=False
         attack_params = {
             'eps' : epsilon,
-            'norm' : np.inf,
-            # 'clip_min' : None,
-            # 'clip_max' : None,
-            # 'y' : None,
+            'norm' : np.inf,    #np.inf, 1, 2
             'targeted' : False,
             'sanity_checks' : False,
         }
     elif attack_name == 'PGD':
         attack = projected_gradient_descent
         attack_params = {
-            'eps': 0.01,
+            'eps': epsilon,
             'eps_iter': 0.01,
             'nb_iter': 15,
             'norm': np.inf,  # Or 1, 2 for l1, l2 norms
@@ -80,10 +75,6 @@ def get_attack_info(attack_name, epsilon):
             'norm' : np.inf,   # 2 or np.inf
             'max_num_evals' : 2000,  # default is 10000
             'constraint': 2,  # 'l2', 'l1', or 'linf'
-            # 'stepsize': 0.5,
-            # 'steps': 40,
-            # 'n_samples': 20,
-            # 'targeted': False,
             'clip_min': 0,
             'clip_max': 1
         }
@@ -108,10 +99,15 @@ def attack(attack_name, attack_params, model, dataloader, brightness_factor=None
     model.to(device)
     adv_images, orig_predicted_classes, adv_predicted_classes = [], [], []
 
+    total_mismatched = 0
+
     for i, batch in enumerate(dataloader):
         images, _ = batch
         images = images.to(device)
+        batch_size = images.shape[0]
         print(f'Working with batch {i+1}')
+        batch_asr = 0 
+        mismatched = 0
 
         for image in images:
             original_image = image.clone().detach().to(device)  # Capture original image
@@ -127,6 +123,10 @@ def attack(attack_name, attack_params, model, dataloader, brightness_factor=None
             _, orig_predicted_class = original_logits.max(1)
             _, adv_predicted_class = adversarial_logits.max(1)
 
+            if orig_predicted_class.item() != adv_predicted_class.item():
+                mismatched += 1  
+                total_mismatched += 1
+
             # Log images based on brightness adjustment
             if brightness_factor != None:
                 wandb.log({"original_image": wandb.Image(denormalize(original_image.cpu())), 
@@ -137,22 +137,13 @@ def attack(attack_name, attack_params, model, dataloader, brightness_factor=None
                             "adversarial_image": wandb.Image(adversarial_image.cpu())})
 
             adv_images.append(adversarial_image)
-            orig_predicted_classes.append(orig_predicted_class)
-            adv_predicted_classes.append(adv_predicted_class)
-        print(f'Adversarial images for batch {i+1} generated successfully\n')
 
-    return adv_images, orig_predicted_classes, adv_predicted_classes
+        batch_asr = mismatched / batch_size * 100
+        wandb.log({'Batch ASR' : batch_asr })
+        print(f'Adversarial images for batch {i+1} generated successfully')
+        print(f'This batch has an ASR: {batch_asr}\n')
 
-
-def evaluate_predictions(original_preds, adversarial_preds):
-
-    mismatched = 0
-    for i in range(len(original_preds)):
-        or_pred = original_preds[i].item()
-        adv_pred = adversarial_preds[i].item()
-        if or_pred != adv_pred:
-            mismatched += 1
-    return mismatched
+    return adv_images, total_mismatched
 
 
 def get_model(model_name):
@@ -175,12 +166,11 @@ def get_model(model_name):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='CleverHans Attacks')
-
-    parser.add_argument('--attack', choices=['FGSM', 'CWl2', 'HOP-SKIP', 'PGD', 'Sl1D'], help='The attack to perform.')
-    parser.add_argument('--image_folder_path', help='Image dataset to perturb', default='../imagenetv2-top-images/imagenetv2-top-images-format-val')
     parser.add_argument('--model', choices=['resnet50', 'resnet152', 'vgg16_bn', 'vit_b_16', 'vit_l_32'], help='Model to attack')
-    # parser.add_argument('--epochs', help='Number of epochs')
-    parser.add_argument('--brightness', help='Brightness value, between 0 (black image) and 2')
+    parser.add_argument('--attack', choices=['FGSM', 'CWl2', 'HOP-SKIP', 'PGD', 'Sl1D'], help='The attack to perform.')
+    parser.add_argument('--epsilon', type=float, help='The perturbation budget, controlling the maximum amount of change allowed to the input. In the range [0,1]', default=0.05)
+    parser.add_argument('--brightness', type=float, help='Brightness value, between 0 (black image) and 2', default=0)
+    parser.add_argument('--image_folder_path', help='Image dataset to perturb', default='../../imagenetv2-top-images/imagenetv2-top-images-format-val')
     parser.add_argument('--additional', help='Any additional comment that will be added to the project name for logging in wandb')
     args = parser.parse_args()
 
@@ -191,24 +181,17 @@ if __name__ == "__main__":
     dataset = datasets.ImageFolder(image_folder, transform=transform())
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-    # No use of multiple epochs, since Cleverhans has non-training methods - but could be used for future improvements.
-    # assert isinstance(args.epochs, int), "Epochs should be an integer"
-    # epochs = int(args.epochs)
-
-    try:
-        brightness_factor = float(args.brightness)
-    except:
-        brightness_factor = None
-    # brightness_factor = None if args.brightness is None or '' else float(args.brightness)
-    additional_comment = args.additional
-    attack_name, attack_params = get_attack_info(attack_name=args.attack, epsilon=0.09)
     model_name = args.model
     model = get_model(model_name).to(device)
     
-    num_of_images = 2000
-    total_mismatch = 0
+    epsilon=args.epsilon
+    attack_name, attack_params = get_attack_info(attack_name=args.attack, epsilon=epsilon)
 
-    project_name = f'Cleverhans {args.attack}-{model_name}{ "(br "+str(brightness_factor)+")" if brightness_factor else ""}{ "_"+additional_comment if additional_comment else ""}'
+    brightness_factor = args.brightness if args.brightness != 0 else None
+    additional_comment = args.additional
+    num_of_images = 100
+
+    project_name = f'Cleverhans {args.attack}-{model_name}_eps_{epsilon}{" (br "+str(brightness_factor)+")" if brightness_factor else ""}{ "_"+additional_comment if additional_comment else ""}'
     wandb.init(project=project_name, config= {
         'model_name': model_name,
         'brightness': args.brightness,
@@ -217,14 +200,10 @@ if __name__ == "__main__":
         **attack_params  # Log the attack parameters
     })
 
-    adv_images, original_preds, adv_preds = attack(attack_name, attack_params, model, dataloader, brightness_factor, device)
-    mismatched = evaluate_predictions(original_preds, adv_preds)
-    total_mismatch += mismatched
-    wandb.log({f'Number of mistmatches on {model_name} model': mismatched})
-    print(f'\n{"="*60}\nNumber of mistmatches on {model_name} model: {mismatched}')
+    adv_images, total_mismatched = attack(attack_name, attack_params, model, dataloader, brightness_factor, device)
+    print(f'\n{"="*60}\nNumber of mistmatches on {model_name} model: {total_mismatched}')
 
-    success_rate = (total_mismatch /  num_of_images) * 100
-    wandb.log({f'Success rate of {args.attack} attack on {model_name} model': success_rate})
-    print(f'Success rate of {args.attack} attack on {model_name} model: {success_rate}\n{"="*60}\n')
+    success_rate = (total_mismatched /  num_of_images) * 100
+    print(f'Success rate of {args.attack} attack on {model_name} model: {success_rate :.2f}%\n{"="*60}\n')
     
     wandb.finish()
