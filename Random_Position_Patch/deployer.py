b108e1422b9cc2e7aacf55b8d0eeb7b444222fe9
Tamar Noselidze <takonoselidze@gmail.com>
2025-05-05 12:08:13 +0200

    removed batch deployment implementation

--- Random_Position_Patch/deployer.py
+++ Random_Position_Patch/deployer.py
@@ -2,7 +2,6 @@ import torch
 
 class Deployer:
     def deploy(self, patch, image):
-        # # print(f'--- SHAPE OF THE PATCH: {patch.shape}')
         _, H, W = image.shape
         _, P_H, P_W = patch.shape
         
@@ -19,24 +18,5 @@ class Deployer:
         adversarial_image = mask + (1 - mask) * image  # combine the mask with the original image
         
         return adversarial_image
-        # B, C, H, W = images.shape
-        # _, _, P_H, P_W = patches.shape
-
-        # adv_images = torch.empty_like(images)
-
-        # for i in range(B):
-        #     image = images[i]
-        #     patch = patches[i]
-
-        #     # Create empty mask
-        #     mask = torch.zeros_like(image)
-
-        #     # Random position
-        #     k = torch.randint(0, H - P_H + 1, (1,)).item()
-        #     l = torch.randint(0, W - P_W + 1, (1,)).item()
-
-        #     # Apply patch
-        #     mask[:, k:k + P_H, l:l + P_W] = patch
-        #     adv_images[i] = mask + (1 - mask) * image
-
-        # return adv_images
+    
+    
